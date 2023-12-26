05cb4224d4fc1103df05b8ded1ffab0cbab6995a
Jaivin Mungara <71208031+jaivin92@users.noreply.github.com>
2023-12-26 13:03:26 +0530

    refactor to use IAuthServices and Auth Services

---
+++ Core/Dtos/AuthServicesResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace jwtAuthAspNet7WebApi.Core.Dtos
+{
+    public class AuthServicesResponseDto
+    {
+        public Boolean IsSuceed { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Core/Interfaces/IAuthServices.cs
@@ -0,0 +1,17 @@
+﻿using jwtAuthAspNet7WebApi.Core.Dtos;
+
+namespace jwtAuthAspNet7WebApi.Core.Interfaces
+{
+    public interface IAuthServices
+    {
+        Task<AuthServicesResponseDto> SeedRolesAsync();
+
+        Task<AuthServicesResponseDto> RegisterAsync(RegisterDto registerDto);
+
+        Task<AuthServicesResponseDto> LoginAsync(LoginDto loginDto);
+
+        Task<AuthServicesResponseDto> MakeAdminAsync(UpdatePermissionDto updatePermissionDto);
+
+        Task<AuthServicesResponseDto> MakeOwnerAsync(UpdatePermissionDto updatePermissionDto);
+    }
+}
